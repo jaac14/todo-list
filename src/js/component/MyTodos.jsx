bbca6199a017744bdc523588eca39625ea87abca
Jose Avendaño <jaac.aven@gmail.com>
2023-10-14 12:40:20 -0400

    primer commit

---
+++ src/js/component/MyTodos.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+//declarando el estado inicial de mi tarea
+const initialState = {
+  label: "",
+  done: false,
+};
+
+const MyTodos = () => {
+  //guardando lo que se copia en mi input value
+  const [task, setTask] = useState("");
+
+  //guardando mi tarea ya lista en el estado que se mostrara
+  const [myListTasks, setMyListTasks] = useState([]);
+
+  //manejando el error
+  const [error, setError] = useState(false);
+
+  //function para fuardar my tarea ya terminada
+
+  function saveMyTaks(event) {
+    if (event.key === "Enter") {
+      if (event.target.value.trim() !== "") {
+        setMyListTasks([...myListTasks, { label: task, done: false }]);
+        setTask("");
+        setError(false);
+      } else {
+        setError(true);
+      }
+    }
+  }
+
+  //add my input value from my input to my state
+  function addInputValue(event) {
+    setTask(event.target.value);
+  }
+
+  //eliminar tareas
+  const deleteTask = (id) => {
+    let newArr = myListTasks.filter((_, index) => index != id);
+
+    setMyListTasks(newArr);
+  };
+
+  return (
+    <div className="Container myTodos">
+      {error ? (
+        <div className="alert alert-danger">
+          Debes ingresar un tipo de dato valido
+        </div>
+      ) : (
+        false
+      )}
+      <ul>
+        <li className="li-input">
+          <input
+            name="label"
+            type="text"
+            placeholder="Add your tasks here "
+            onChange={addInputValue}
+            onKeyDown={saveMyTaks}
+            value={task}
+          />
+        </li>
+
+        {myListTasks.map((item, index) => {
+          return (
+            <li key={index} className="list-task">
+              {item.label}
+              <i
+                className="fa-solid fa-trash-can fa-beat-fade"
+                onClick={() => deleteTask(index)}
+              ></i>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default MyTodos;
